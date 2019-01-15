f2c6535ccbc218a5b295161e69c3f332892a5995
secilreel <45249969+secilreel@users.noreply.github.com>
2019-01-15 11:30:34 -0500

    Update drills.js

---
+++ drills.js
@@ -0,0 +1,13 @@
+/* What is scope?
+Scope is like where variables can be accessed.
+In global it could be anywhere in the file. In local scope, variable is only within the function block. 
+Why is global scope important?
+A global scope can mess up with the function as the function looks up to the parent. 
+The function can use a global variable accidentally
+Strict mode?
+It forces all variables defined within a function 
+block to be a local variable. It prevents the hoisting up. 
+If you declare with var instead of let or const, it throws an error.
+side effects?
+When a function reaches out of its local scope and changes something in global scope.
+In that case, a function will give us inconsistent results every time we run it. Pure functions has no side effects. */
